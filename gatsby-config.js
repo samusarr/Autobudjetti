9e1b8ac088248993bfaf8989c9c89c2bccb0eecc
unknown <sarresamu@gmail.com>
2020-08-04 16:35:28 +0300

    manifest fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,7 @@ module.exports = {
         background_color: ``,
         theme_color: ``,
         display: ``,
-        icon: `icons/autobudjetti-icon.png`, // This path is relative to the root of the site.
+        icon: `src/icons/autobudjetti-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
