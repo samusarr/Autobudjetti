22b31f019758666bc9bdcf01944ed9942a65053c
unknown <sarresamu@gmail.com>
2020-08-04 16:44:34 +0300

    fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,18 +16,18 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-smoothscroll`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `autobudjetti`,
-        short_name: `autobudjetti`,
-        start_url: `/`,
-        background_color: ``,
-        theme_color: ``,
-        display: ``,
-        icon: `src/icons/autobudjetti-icon.png`, // This path is relative to the root of the site.
-      },
-    },
+    // {
+    //   resolve: `gatsby-plugin-manifest`,
+    //   options: {
+    //     name: `autobudjetti`,
+    //     short_name: `autobudjetti`,
+    //     start_url: `/`,
+    //     background_color: ``,
+    //     theme_color: ``,
+    //     display: ``,
+    //     icon: `src/icons/autobudjetti-icon.png`, // This path is relative to the root of the site.
+    //   },
+    // },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
